1ce5d94c057cc9a79bc08450f169cac5f3ce89da
eandres <eandres@student.42urduliz.com>
2025-11-17 17:47:27 +0100

    update

--- assignment04/main.c
+++ assignment04/main.c
@@ -4,12 +4,16 @@
 #include <linux/kernel.h>
 #include <linux/usb.h>
 #include <linux/mod_devicetable.h>
+#include <linux/hid.h>
 
 MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Your Name");
+MODULE_DESCRIPTION("USB Keyboard Detector for Assignment 04");
 
 static struct usb_device_id my_usb_id_table[] = {
-	{ USB_DEVICE(0x80ee, 0x0021) },
-
+	{
+		USB_DEVICE(0x80ee, 0x0021)
+	},
 	{ }
 
 };
@@ -18,17 +22,17 @@ MODULE_DEVICE_TABLE(usb, my_usb_id_table);
 
 static int my_usb_probe(struct usb_interface *interface, const struct usb_device_id *id)
 {
-	printk(KERN_DEBUG "USB Detector: !Se ha CONECTADO el raton");
-	return -ENODEV;
+	printk(KERN_DEBUG "USB Detector: Keyboard CONNECTED\n");
+	return 0;
 }
 
 static void my_usb_disconnect(struct usb_interface *interface)
 {
-	printk(KERN_DEBUG "USB Detector: !Se ha DESCONECTADO el raton");
+	printk(KERN_DEBUG "USB Detector: Keyboard DISCONNECTED\n");
 }
 
 static struct usb_driver my_usb_driver = {
-	.name = "usb_driver_kernel",
+	.name = "my_usb_keyboard_driver",
 	.id_table = my_usb_id_table,
 	.probe = my_usb_probe,
 	.disconnect = my_usb_disconnect,
@@ -40,16 +44,16 @@ static int	__init usb_driver_init(void)
 
 	result = usb_register(&my_usb_driver);
 	if (result)
-		printk(KERN_DEBUG "USB Detector: Fallo el registro del driver.\n");
+		printk(KERN_DEBUG "USB Detector: Driver registration failed. Errorr %d\n", result);
 	else
-		printk(KERN_DEBUG "USB Detector: Driver registrado.\n");
+		printk(KERN_DEBUG "USB Detector: Driver registered.\n");
 	return result;
 }
 
 static void	__exit usb_driver_exit(void)
 {
 	usb_deregister(&my_usb_driver);
-	pr_info("Cleaning up module.\n");
+	printk(KERN_DEBUG "Cleaning up module.\n");
 }
 
 module_init(usb_driver_init);
