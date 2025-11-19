a4692016e5aba2580ca0326617348f3a85f272cd
eandres <eandres@student.42urduliz.com>
2025-11-19 13:40:19 +0100

    update, assignment07

--- assignment07/main.c
+++ assignment07/main.c
@@ -14,24 +14,84 @@ MODULE_AUTHOR("eandres");
 MODULE_DESCRIPTION("Debugfs");
 
 static struct dentry *dir = NULL;
+static struct mutex foo_mutex;
+static	char foo_data[4096];
 
-static ssize_t my_read(struct file *f, char __user *user_buf, size_t count, loff_t *f_pos)
+static ssize_t id_read(struct file *f, char __user *user_buf, size_t count, loff_t *f_pos)
 {
-	
+	char *login = "eandres\n";
+	size_t len = 9;
+
+	return simple_read_from_buffer(user_buf, count, f_pos, login, len);
 }
 
-static ssize_t my_write(struct file *f, char __user *user_buf, size_t count, loff_t *f_pos)
+static ssize_t id_write(struct file *f, char __user *user_buf, size_t count, loff_t *f_pos)
 {
+	char kbuf[32];
+	ssize_t copied;
+	char *my_login = "eandres";
+	int login_len = 8;
 
+	copied = simple_write_to_buffer(kbuf, sizeof(kbuf) -1, f_pos, user_buf, count);
+	if (copied < 0)
+		return copied;
+
+	if (strncmp(kbuf, my_login, login_len) == 0)
+		return copied;
+	else
+		return -EINVAL;
 }
 
-static struct file_operations read_and_write = {
+static ssize_t jiffies_read(struct file *f, char __user *user_buf, size_t count, loff_t *f_pos)
+{
+	// Crear un buffer temporal para guardar el texto
+	char temp_buffer[32];
+	int len;
+
+	// Convertir el numbero 'jiffies' a texto
+	// Guardamos el resultado en temp_buffer
+	len = snprintf(temp_buffer, sizeof(temp_buffer), "%lu\n", jiffies);
+
+	// Enviar este buffer de texto al usuario
+	// simple_read_from_buffer hace la copia segura al espacio de usuario
+	// y gestiona el offset (f_pos)
+	return simple_read_from_buffer(user_buf, count, f_pos, temp_buffer, len);
+}
+
+static ssize_t foo_read(struct file *f, char __user *user_buf, size_t count, loff_t *f_pos)
+{
+	mutex_lock(&foo_mutex);
+	ssize_t result = simple_write_from_buffer(user_buf, count, f_pos, fs, sizeof(foo_data));
+	mutex_unlock(&foo_mutex);
+	return result;
+}
+
+static ssize_t foo_write(struct file *f, char __user *user_buf, size_t count, loff_t *f_pos)
+{
+	mutex_lock(&foo_mutex);
+	ssize_t result = simple_read_from_buffer(foo_data, sizeof(foo_data) -1, f_pos, user_buf, count);
+	mutex_unlock(&foo_mutex);
+	return 0;
+}
+
+static struct file_operations fops_id = {
 	.owner = THIS_MODULE,
-	.read = my_read,
-	.write = my_write,
+	.read = id_read,
+	.write = id_write,
 };
 
-static int	create_dir_and_file()
+static struct file_operations fops_jiffies = {
+	.owner = THIS_MODULE,
+	.read = jiffies_read,
+};
+
+static struct file_operations fops_foo = {
+	.owner = THIS_MODULE,
+	.read = foo_read,
+	.write = foo_write,
+};
+
+static int	__init my_module_init(void)
 {
 	dir = debugfs_create_dir("fortytwo", NULL);
 	if (IS_ERR(dir))
@@ -40,18 +100,26 @@ static int	create_dir_and_file()
 		return 0;
 	}
 
-	struct dentry *file_id = debugfs_create_file("id", 0666, dir, NULL, &read_and_write)
-	if (IS_ERR(filie_id))
+	struct dentry *file_id = debugfs_create_file("id", 0666, dir, NULL, &fops_id);
+	if (IS_ERR(file_id))
 	{
-		pr_infor("Failure to create file");
+		pr_info("Failure to create file");
 		return 0;
 	}
-}
 
-static int	__init my_module_init(void)
-{
-	if (create_dir_and_file() == 0)
-		return PTR_ERR;
+	file_id = debugfs_create_file("jiffies", 0444, dir, NULL, &fops_jiffies);
+	if (IS_ERR(file_id))
+	{
+		pr_info("Failure to create file");
+		return 0;
+	}
+
+	file_id = debugfs_create_file("foo", 0600, dir, NULL, &fops_foo);
+	if (IS_ERR(file_id))
+	{
+		pr_info("Failure to create file");
+		return 0;
+	}
 	return 0;
 }
 
