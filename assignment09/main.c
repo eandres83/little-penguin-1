2af9d8edd56773f2255a562c97a5c725f0db59b9
eandres <eandres@student.42urduliz.com>
2025-11-27 19:17:30 +0100

    update main

--- assignment09/main.c
+++ assignment09/main.c
@@ -1,13 +1,47 @@
 // SPDX-License-Identifier: GPL-2.0
 
 #include <linux/fs.h>
+#include <linux/module.h>
 #include <linux/init.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
+#include <linux/nsproxy.h>
+#include <linux/sched.h>
+#include <linux/mount.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("eandres");
 MODULE_DESCRIPTION("Module creates a file in procfs and show");
+
+struct mnt_namespace {
+	atomic_t count;
+	struct ns_common ns;
+	struct mount *root;
+	struct list_head list;
+};
+
+struct mount {
+	struct hlist_mode mnt_hash;
+	struct mount *mnt_parent;
+	struct dentry *mnt_mountpoint;
+	struct vfsmount mnt;
+	union {
+		struct rcu_head mnt_rcu;
+		struct llist_node mnt_llist;
+	};
+#ifdef CONFIG_SMP
+	struct llist_node mnt_llist_extra;
+#endif
+	struct list_head mnt_list;
+	struct list_head mnt_child;
+	struct list_head mnt_instance;
+	const char *mnt_devname;
+	struct list_head mnt_share;
+	struct list_head mnt_slave_list;
+	struct mount *mnt_master;
+	struct mnt_namespace *mnt_ns;
+	struct mountpoint *mnt_mp;
+};
 
 static int my_show(struct seq_file *m, void *v)
 {
@@ -20,7 +54,7 @@ static int my_show(struct seq_file *m, void *v)
 	// Iterar sobre la lista de montajes
 	list_for_each_entry(mnt, &ns->list, mnt_list) {
 		// Imprimir el nombre del dispositivo y el punto de montaje
-		seq_printf(m, "%-20s\t", mnt->mnt_devname ? mnt->mnt_davname : "none");
+		seq_printf(m, "%-20s\t", mnt->mnt_devname ? mnt->mnt_devname : "none");
 		seq_path(m, &mnt->mnt.mnt_root->d_path, "\n");
 	}
 
