21ef8522a66081171c57ea46387cba3f18500e54
eandres <eandres@student.42urduliz.com>
2025-11-28 15:18:25 +0100

    update with proc_ops assignment09

--- assignment09/main.c
+++ assignment09/main.c
@@ -1,82 +1,52 @@
 // SPDX-License-Identifier: GPL-2.0
 
-#include <linux/fs.h>
 #include <linux/module.h>
-#include <linux/init.h>
+#include <linux/fs.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
 #include <linux/nsproxy.h>
-#include <linux/sched.h>
-#include <linux/mount.h>
+#include <linux/ns_common.h>
+#include <../fs/mount.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("eandres");
 MODULE_DESCRIPTION("Module creates a file in procfs and show");
 
-struct mnt_namespace {
-	atomic_t count;
-	struct ns_common ns;
-	struct mount *root;
-	struct list_head list;
-};
-
-struct mount {
-	struct hlist_mode mnt_hash;
-	struct mount *mnt_parent;
-	struct dentry *mnt_mountpoint;
-	struct vfsmount mnt;
-	union {
-		struct rcu_head mnt_rcu;
-		struct llist_node mnt_llist;
-	};
-#ifdef CONFIG_SMP
-	struct llist_node mnt_llist_extra;
-#endif
-	struct list_head mnt_list;
-	struct list_head mnt_child;
-	struct list_head mnt_instance;
-	const char *mnt_devname;
-	struct list_head mnt_share;
-	struct list_head mnt_slave_list;
-	struct mount *mnt_master;
-	struct mnt_namespace *mnt_ns;
-	struct mountpoint *mnt_mp;
-};
-
 static int my_show(struct seq_file *m, void *v)
 {
 	struct mount *mnt;
-	struct mnt_namespace *ns;
+	char buf[256];
+	char *p;
 
-	// Obtener el namespace de montaje del proceso actual
-	ns = current->nsproxy->mnt_ns;
-
-	// Iterar sobre la lista de montajes
-	list_for_each_entry(mnt, &ns->list, mnt_list) {
-		// Imprimir el nombre del dispositivo y el punto de montaje
-		seq_printf(m, "%-20s\t", mnt->mnt_devname ? mnt->mnt_devname : "none");
-		seq_path(m, &mnt->mnt.mnt_root->d_path, "\n");
+	list_for_each_entry(mnt, &current->nsproxy->mnt_ns->list, mnt_list) {
+		p = dentry_path_raw(mnt->mnt_mountpoint, buf, sizeof(buf));
+		seq_printf(m, "%-15%s\t", mnt->mnt_devname, p);
 	}
 
 	return (0);
 }
 
+static int mymounts_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, my_show, NULL);
+}
+
+static const struct proc_ops mymounts_fops = {
+	.proc_open = mymounts_open,
+	.proc_read = seq_read,
+	.proc_lseek = seq_lseek,
+	.proc_release = single_release,
+};
+
 static int __init proc_init(void)
 {
-	struct proc_dir_entry *pde;
-
-	pde = proc_create_single("mymounts", 0, NULL, my_show);
-	if (!pde) {
-		return -ENOMEM;
-	}
-
+	proc_create("mymounts", 0666, NULL, &mymounts_fops);
 	return (0);
 }
 
 static void __exit proc_exit(void)
 {
 	remove_proc_entry("mymounts", NULL);
-	pr_info("Remove mymounts\n");
 }
 
 module_init(proc_init);
