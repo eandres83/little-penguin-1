a13175ad0d159dc5bd003be51d3b0fd6123242cd
Eleder Andres <eandres@c3r8s6.42urduliz.com>
2025-12-08 18:47:14 +0100

    update conding style in assgnment09 and assignment08

--- assignment08/main.c
+++ assignment08/main.c
@@ -12,12 +12,14 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Louis Solofrizzo <louis@ne02ptzero.me>");
 MODULE_DESCRIPTION("Useless module");
 
-static ssize_t myfd_read(struct file *fp, char __user *user, size_t size, loff_t *offs);
-static ssize_t myfd_write(struct file *fp, const char __user *user, size_t size, loff_t *offs);
+static ssize_t myfd_read(struct file *fp, char __user *user,
+			 size_t size, loff_t *offs);
+static ssize_t myfd_write(struct file *fp, const char __user *user,
+			  size_t size, loff_t *offs);
 
 static const struct file_operations myfd_fops = {
 	.owner = THIS_MODULE,
-	.read = &myfd_read
+	.read = &myfd_read,
 	.write = &myfd_write,
 };
 
@@ -28,42 +30,56 @@ static struct miscdevice myfd_device = {
 };
 
 char str[PAGE_SIZE];
-char *tmp;
+
+ssize_t myfd_read(struct file *fp, char __user *user, size_t size, loff_t *offs)
+{
+	size_t len;
+	size_t i;
+	ssize_t ret;
+	char *tmp2;
+
+	len = strlen(str);
+
+	if (len == 0)
+		return 0;
+	tmp2 = kmalloc(len + 1, GFP_KERNEL);
+	if (!tmp2)
+		return -ENOMEM;
+	for (i = 0; i < len; i++)
+		tmp2[i] = str[len - 1 - i];
+	tmp2[len] = '\0';
+	ret = simple_read_from_buffer(user, size, offs, tmp2, len);
+	kfree(tmp2);
+	return ret;
+}
+
+ssize_t myfd_write(struct file *fp, const char __user *user, size_t size,
+		   loff_t *offs)
+{
+	ssize_t res;
+
+	res = 0;
+	res = simple_write_to_buffer(str, PAGE_SIZE - 1, offs, user, size);
+	if (res > 0)
+		// 0x0 = '\0'
+		str[size + 1] = 0x0;
+	return res;
+}
 
 static int __init myfd_init(void)
 {
 	int retval;
 
-	retval = misc_register(&(*(&(myfd_device))));
+	memset(str, 0, PAGE_SIZE);
+	retval = misc_register(&myfd_device);
 	return 1;
 }
 
 static void __exit myfd_cleanup(void)
 {
+	misc_deregister(&myfd_device);
 }
 
-ssize_t myfd_read(struct file *fp, char __user *user, size_t size, loff_t *offs)
-{
-	size_t t, i;
-	char *tmp2;
-
-	tmp2 = kmalloc(sizeof(char) * PAGE_SIZE * 2, GFP_KERNEL);
-	tmp = tmp2;
-	for (t = strlen(str) - 1, i = 0; t >= 0; t--, i++)
-		tmp[i] = str[i];
-	tmp[i] = 0x0;
-	return simple_read_from_buffer(user, size, offs, tmp, i);
-}
-
-ssize_t myfd_write(struct file *fp, const char __user *user, size_t size, loff_t *offs)
-{
-	ssize_t res;
-
-	res = 0;
-	res = simple_write_to_buffer(str, size, offs, user, size) + 1;
-	str[size + 1] = 0x0;
-	return res;
-}
 module_init(myfd_init);
 module_exit(myfd_cleanup);
 
--- assignment09/main.c
+++ assignment09/main.c
@@ -12,73 +12,98 @@
 #include <linux/sched.h>
 #include <linux/list.h>
 
-struct mnt_namespace {
-	atomic_t				count;
-	struct ns_common		ns;
-	struct mount			*root;
-	struct list_head		list; // <--- El campo que buscabas
-	struct user_namespace   *user_ns;
-	struct ucounts		  *ucounts;
-	u64					 seq;
-};
-
-/* * Definimos struct mount. 
- * IMPORTANTE: Esta estructura es GIGANTE. Solo definimos el principio 
- * para acceder a mnt_mountpoint y mnt_list.
- * Para acceder a mnt_devname, necesitamos trucos o la struct completa.
- * Esta versión simplificada suele funcionar para los primeros campos.
- */
-struct mount {
-	struct hlist_node mnt_hash;
-	struct mount *mnt_parent;
-	struct dentry *mnt_mountpoint;
-	struct mount *mnt_master;
-	struct mount *mnt_slave;
-	struct mount *mnt_share;
-	struct list_head mnt_slave_list;
-	struct list_head mnt_share_list;
-	struct list_head mnt_expire;
-	struct list_head mnt_share;
-	struct list_head mnt_list;
-	struct list_head mnt_expire_list;
-	struct list_head mnt_share_list;
-	struct list_head mnt_slave_list;
-	struct list_head mnt_list_all;
-	struct mount *mnt_ex_mountpoint;
-	struct fs_struct *mnt_fs;
-	/* * OJO: mnt_devname suele estar MUY abajo en la estructura. 
-	 * Acceder a él directamente con una estructura incompleta causará SEGFAULT.
-	 * Para este ejercicio, intenta acceder a él a través del superbloque si es posible,
-	 * o usa esta definición extendida "a ojo":
-	 */
-	 int mnt_id;
-	 int mnt_group_id;
-	 int mnt_expiry_mark;
-	 int mnt_flags;
-	 /* ... mucho relleno aquí ... */
-	 /* Si falla, borra el acceso a mnt_devname en tu código */
-	 const char *mnt_devname; 
-};
-
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("eandres");
 MODULE_DESCRIPTION("Module creates a file in procfs and show");
 
+struct mnt_namespace {
+	struct ns_common	ns;
+	struct mount		*root;
+	struct list_head	list;
+	struct user_namespace   *user_ns;
+	struct ucounts		*ucounts;
+	u64			 seq;
+};
+
+struct mount {
+	struct hlist_node mnt_hash;
+	struct mount *mnt_parent;
+	struct dentry *mnt_mountpoint;
+	struct vfsmount mnt;
+	union {
+		struct rb_node mnt_node; /* node in the ns->mounts rbtree */
+		struct rcu_head mnt_rcu;
+		struct llist_node mnt_llist;
+	};
+#ifdef CONFIG_SMP
+	struct mnt_pcp __percpu *mnt_pcp;
+#else
+	int mnt_count;
+	int mnt_writers;
+#endif
+	struct list_head mnt_mounts;	/* list of children, anchored here */
+	struct list_head mnt_child;	/* and going through their mnt_child */
+	struct list_head mnt_instance;	/* mount instance on sb->s_mounts */
+	struct list_head mnt_list;
+	struct list_head mnt_expire;	/* link in fs-specific expiry list */
+	struct list_head mnt_share;	/* circular list of shared mounts */
+	struct list_head mnt_slave_list;/* list of slave mounts */
+	struct list_head mnt_slave;	/* slave list entry */
+	struct mount *mnt_master;	/* slave is on master->mnt_slave_list */
+	struct mnt_namespace *mnt_ns;	/* containing namespace */
+	struct mountpoint *mnt_mp;	/* where is it mounted */
+	union {
+		struct hlist_node mnt_mp_list;	/* list mounts with the same mountpoint */
+		struct hlist_node mnt_umount;
+	};
+	struct list_head mnt_umounting; /* list entry for umount propagation */
+#ifdef CONFIG_FSNOTIFY
+	struct fsnotify_mark_connector __rcu *mnt_fsnotify_marks;
+	__u32 mnt_fsnotify_mask;
+	struct list_head to_notify;	/* need to queue notification */
+	struct mnt_namespace *prev_ns;	/* previous namespace (NULL if none) */
+#endif
+	int mnt_id;			/* mount identifier, reused */
+	u64 mnt_id_unique;		/* mount ID unique until reboot */
+	int mnt_group_id;		/* peer group identifier */
+	int mnt_expiry_mark;		/* true if marked for expiry */
+	struct hlist_head mnt_pins;
+	struct hlist_head mnt_stuck_children;
+} __randomize_layout;
+
+static void print_mount_recursive(struct mount *mnt, struct seq_file *m, char *buf, int len)
+{
+	struct super_block *sb;
+	char *p;
+	struct mount *child;
+
+	// conseguir la ruta actual y imprimirla
+	p = dentry_path_raw(mnt->mnt_mountpoint, buf, len);
+	if (!IS_ERR(p)) {
+		sb = mnt->mnt.mnt_sb;
+		if (sb) {
+			if (sb->s_id[0])
+				seq_printf(m, "%-15s %s\n", sb->s_id, p);
+			else
+				seq_printf(m, "%-15s %s\n", "none", p);
+		}
+	}
+	// recurvidad para recorrer todos los hijos
+	list_for_each_entry(child, &mnt->mnt_mounts, mnt_child)
+		print_mount_recursive(child, m, buf, len);
+}
+
 static int my_show(struct seq_file *m, void *v)
 {
-	struct mount *mnt;
+	struct mount *root_mnt;
 	char buf[256];
-	char *p;
 
-	list_for_each_entry(mnt, &current->nsproxy->mnt_ns->list, mnt_list) {
-		p = dentry_path_raw(mnt->mnt_mountpoint, buf, sizeof(buf));
-		char *devname = "none";
-		if (mnt->mnt_mountpoint && mnt->mnt_mountpoint->d_sb)
-			devname = mnt->mnt_mountpoint->d_sb->s_id;
-		seq_printf(m, "%-15s\t%s\n", devname, p);
-	}
+	root_mnt = current->nsproxy->mnt_ns->root;
 
-	return (0);
+	if (root_mnt)
+		print_mount_recursive(root_mnt, m, buf, sizeof(buf));
+
+	return 0;
 }
 
 static int mymounts_open(struct inode *inode, struct file *file)
@@ -97,14 +122,13 @@ static int __init proc_init(void)
 {
 	struct proc_dir_entry *mymounts;
 
-	mymounts = proc_create("mymounts", 0666, NULL, &mymounts_fops);
-	if (!mymounts)
-	{
+	mymounts = proc_create("mymounts", 0644, NULL, &mymounts_fops);
+	if (!mymounts) {
 		pr_info("Error when creatin the proc device.\n");
 		return -ENOMEM;
 	}
 	pr_info("Proc device created.\n");
-	return (0);
+	return 0;
 }
 
 static void __exit proc_exit(void)
